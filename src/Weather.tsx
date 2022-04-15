7b9118bf0722526ff3e257bbbf8b66e111307dea
Jason Aravanis <j.aravanis@icloud.com>
2022-04-15 20:58:52 +1000

    Add weather design mock

--- src/Weather.tsx
+++ src/Weather.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./styles/weather-icons.min.css";
 
 // refresh every five minutes.
 const refreshInterval = 1000 * 60 * 5;
@@ -29,7 +30,75 @@ const Weather = () => {
     return () => clearInterval(interval);
   }, [fetchedWeather]);
 
-  return <div>{fetchedWeather?.current.dew_point}</div>;
+  return (
+    <div className=" flex flex-col">
+      <div className="  flex justify-center items-center">
+        <div>
+          <i className="wi wi-night-sleet text-9xl" />
+        </div>
+        <div className="ml-5 flex flex-col items-center">
+          <span className="text-8xl">19°</span>
+          <div className="flex">
+            <span className="text-4xl m-4">25°</span>
+            <span className="text-4xl m-4 font-thin">9°</span>
+          </div>
+        </div>
+      </div>
+      <span className=" text-5xl my-5">Light rain</span>
+      <div className=" text-3xl">
+        <span>
+          <i className="wi wi-sunrise"></i>
+          <span>6:35 am</span>
+        </span>
+        <span className="ml-5">
+          <i className="wi wi-sunset"></i>
+          <span>6:32 pm</span>
+        </span>
+      </div>
+
+      <div className="grid grid-cols-1 grid-rows-4 bg-green-300 text-4xl my-5 gap-2">
+        <div className="w-full bg-purple-300 flex items-center">
+          <span className="w-[30%] bg-red-300">Sat</span>
+          <i className="w-[30%] bg-blue-300 wi wi-night-sleet" />
+          <span className="w-[20%] bg-rose-500">25</span>
+          <span className="w-[20%] bg-orange-300">9</span>
+        </div>
+        <div className="w-full bg-purple-300 flex items-center">
+          <span className="w-[30%] bg-red-300">Sun</span>
+          <i className="w-[30%] bg-blue-300 wi wi-day-sunny" />
+          <span className="w-[20%] bg-rose-500">44</span>
+          <span className="w-[20%] bg-orange-300">1</span>
+        </div>
+        <div className="w-full bg-purple-300 flex items-center">
+          <span className="w-[30%] bg-red-300">Mon</span>
+          <i className="w-[30%] bg-blue-300 wi wi-day-rain" />
+          <span className="w-[20%] bg-rose-500">3</span>
+          <span className="w-[20%] bg-orange-300">18</span>
+        </div>
+      </div>
+
+      {/* <table className="w-full my-5 text-4xl">
+        <tbody>
+          <tr className="">
+            <td>Sat</td>
+            <td>
+              <i className="wi wi-night-sleet" />
+            </td>
+            <td>25</td>
+            <td className="font-thin">9</td>
+          </tr>
+          <tr>
+            <td>Sun</td>
+            <td>
+              <i className="wi wi-day-sunny" />
+            </td>
+            <td>26</td>
+            <td className="font-thin">7</td>
+          </tr>
+        </tbody>
+      </table> */}
+    </div>
+  );
 };
 
 export default Weather;
