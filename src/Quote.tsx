77b43fc21c03dcfa45d35407bb97e821d821baa0
Jason Aravanis <j.aravanis@icloud.com>
2022-04-16 07:42:34 +1000

    Add quote module design

---
+++ src/Quote.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Quote = () => {
+  return (
+    <div className=" flex flex-col justify-center items-center p-10">
+      <span className=" text-4xl text-center">
+        If you love somebody, let them go, for if they return, they were always
+        yours. If they don't, they never were.
+      </span>
+      <div className="mt-5">
+        <span className="text-3xl">Mark Twain</span>
+      </div>
+    </div>
+  );
+};
+
+export default Quote;
